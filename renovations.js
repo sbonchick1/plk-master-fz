bb4a13d776708e6ffaa14939f29f8a25a49fdf47
sbonchick1 <sbonchick@gmail.com>
2026-04-20 11:50:37 -0400

    Add files via upload

---
+++ renovations.js
@@ -0,0 +1,33 @@
+const https = require('https');
+
+const SS_TOKEN    = 'Qcv9OFzbweKqbamkePSG2WpggSbTkEmASUgh1';
+const SS_SHEET_ID = '3928541238349700';
+
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  if (req.method === 'OPTIONS') { res.status(204).end(); return; }
+
+  const options = {
+    hostname: 'api.smartsheet.com',
+    path: `/2.0/sheets/${SS_SHEET_ID}`,
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${SS_TOKEN}`,
+      'Accept': 'application/json'
+    }
+  };
+
+  const proxy = https.request(options, (ssRes) => {
+    let data = '';
+    ssRes.on('data', chunk => data += chunk);
+    ssRes.on('end', () => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(ssRes.statusCode).send(data);
+    });
+  });
+  proxy.on('error', e => {
+    res.status(500).json({ error: e.message });
+  });
+  proxy.end();
+};
